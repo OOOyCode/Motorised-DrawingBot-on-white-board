a487f839acfdce1b346f66c7655fcd4dc68c54ae
OOOyCode <epice8303@gmail.com>
2026-04-25 12:24:19 +0200

    Update ui.py

--- Python/ui.py
+++ Python/ui.py
@@ -1,6 +1,6 @@
 import cv2
 
-def ui(frame, mode):
+def ui(frame, mode, r, g, b):
     cv2.rectangle(frame, (5, 5), (300, 350), (0, 0, 0), -1)
 
     cv2.putText(frame, f"Mode: {mode}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
@@ -9,3 +9,6 @@ def ui(frame, mode):
     cv2.putText(frame, f"Eraser Mode: 'X'", (10, 170), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     cv2.putText(frame, f"Save: 'S'", (10, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     cv2.putText(frame, f"Quit Mode: 'Q'", (10, 290), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    
+    color = (b, g, r) 
+    cv2.rectangle(frame, (frame.shape[0]-100, 10), (frame.shape[0], 110), color, -1)
